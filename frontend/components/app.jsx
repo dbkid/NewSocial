439ca28dd32f270d711852a4bfb9c7275fbec961
dbkid <dbkeefe@gmail.com>
2016-10-05 14:10:01 -0400

    basic header component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from "./header/header_container";
 
 const App = ({ children }) => (
   <div>
     <h1>New Social</h1>
+    <Header/>
     { children }
   </div>
 );
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Link } from 'react-router';
 
@@ -12,30 +11,36 @@ class Header extends React.Component {
     if (this.props.currentUser === null){
 
       return(
-        <ul>
-          <li><img className="logo"></img></li>
-          <li className="NewSocial">NewSocial</li>
-        </ul>
-        <ul>
-          <li><Link to="/#/signup" className="greenlink">Sign Up</Link></li>
-          <li><Link to="/#/login" className="greenlink">Sign In</Link></li>
-        </ul>
+        <div>
+          <ul>
+            <li><img className="logo"></img></li>
+            <li className="newsocial">NewSocial</li>
+          </ul>
+
+          <ul>
+            <li><Link to="/#/signup" className="greenlink">Sign Up</Link></li>
+            <li><Link to="/#/login" className="greenlink">Sign In</Link></li>
+          </ul>
+        </div>
       );
     } else {
 
       return(
-        <ul>
-          <li><img className="logo"></img></li>
-          <li className="NewSocial">NewSocial</li>
-        </ul>
-        <ul>
-          <li><Link to="/#/writestory" className="greenlink">Write Story</Link></li>
-        </ul>
-      )
+        <div>
+          <ul>
+            <li><img className="logo"></img></li>
+            <li className="NewSocial">NewSocial</li>
+          </ul>
+          <ul>
+            <li><Link to="/#/writestory" className="greenlink">Write Story</Link></li>
+          </ul>
+        </div>
+      );
 
 
     }
 
+  }
 }
 
 export default Header;
